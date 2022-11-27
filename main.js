4d9438f061e971ef38cb157c13478e8e180ec39e
James <jjguevarag@gmail.com>
2022-11-27 12:56:57 -0600

    update main

---
+++ main.js
@@ -0,0 +1,27 @@
+// Ejercicio 1:
+// Escriba un programa que imprima, utilizando ciclos, un cuadrado de 10x10
+// caracteres, formado por 2 triángulos, uno con el carácter '#' y el otro con el carácter
+// '*':
+// **********
+// *********#
+// ********##
+// *******###
+// ******####
+// *****#####
+// ****######
+// ***#######
+// **########
+// *#########
+
+// Ejercicio 2:
+// Escriba una función que calcule el valor de un número N elevado por otro número
+// M. (La función debe de recibir ambos parámetros).
+
+// Ejercicio 3:
+// Escriba una función que resuelve el problema del ejercicio anterior de manera
+// recursiva.
+
+
+// Ejercicio 4:
+// Escriba una función recursiva que reciba un arreglo y calcule la suma de sus
+// elementos.
